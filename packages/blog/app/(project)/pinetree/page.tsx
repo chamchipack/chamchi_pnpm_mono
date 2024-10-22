57b25ade02043795b94c9819b911392695ed74a0
chanik <chanik@chanik.gractor.com>
2024-10-22 15:03:23 +0900

    seo

--- packages/blog/app/(project)/pinetree/page.tsx
+++ packages/blog/app/(project)/pinetree/page.tsx
@@ -1,4 +1,14 @@
+import { Metadata } from 'next';
 import ContentListContainer from '@/components/main/server/ContentListContainer';
+
+export const metadata: Metadata = {
+  title: '블로그 참치',
+  description: '내가 만든 내 세상. 블로그 참치',
+  keywords: ['포트폴리오', '블로그', 'blog', '작성'],
+  alternates: {
+    canonical: 'https://blog.chamchipack.com',
+  },
+};
 
 export default function Page() {
   return <ContentListContainer />;
