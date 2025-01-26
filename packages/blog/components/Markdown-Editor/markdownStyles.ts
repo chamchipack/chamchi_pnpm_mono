aca2332ae2513389ac67f71a5fd8e3b422c2c4f4
조찬익 <fnjels09@gmail.com>
2025-01-26 17:14:34 +0900

    [blog] markdown style change

--- packages/blog/components/Markdown-Editor/markdownStyles.ts
+++ packages/blog/components/Markdown-Editor/markdownStyles.ts
@@ -1,16 +1,16 @@
 export const markdownHeaderStyles = {
   '& h1': {
-    marginTop: '25px',
+    marginTop: '30px',
     marginBottom: '25px',
     lineHeight: '1.2',
     wordBreak: 'break-word',
   },
-  '& h2': { marginTop: '18px', marginBottom: '18px' },
-  '& h3': { marginTop: '16px', marginBottom: '16px' },
+  '& h2': { marginTop: '24px', marginBottom: '18px' },
+  '& h3': { marginTop: '20px', marginBottom: '16px' },
   '& h4': { marginTop: '14px', marginBottom: '14px' },
   '& h5': { marginTop: '12px', marginBottom: '12px' },
   '& h6': { marginTop: '10px', marginBottom: '10px' },
-  '& p': { marginTop: '8px', marginBottom: '8px', color: '#434C58' },
+  '& p': { marginTop: '24px', marginBottom: '24px', color: '#434C58' },
   '& strong': {
     fontWeight: 'bold', // 기본적으로 strong 태그는 굵게 표시
     color: '#black', // strong 태그의 텍스트 색상 설정
@@ -24,10 +24,14 @@ export const markdownHeaderStyles = {
     borderLeft: '4px solid #e0e0e0',
     paddingLeft: 2,
     color: '#6a737d',
-    marginTop: '8px',
-    marginBottom: '8px',
+    marginTop: '16px',
+    marginBottom: '16px',
     backgroundColor: '#f6f8fa',
     whiteSpace: 'pre-wrap',
+  },
+  '& blockquote p': {
+    marginTop: '4px', // blockquote 안의 p 태그 마진
+    marginBottom: '4px',
   },
   '& code': {
     color: '#yellow',
@@ -50,6 +54,12 @@ export const markdownHeaderStyles = {
     '&:hover': {
       color: '#ff4500',
     },
+  },
+  '& ol': {
+    paddingLeft: '20px', // 리스트 항목에 왼쪽 여백을 줍니다.
+    marginTop: '10px',
+    marginBottom: '10px',
+    listStyleType: 'decimal', // 불릿 스타일 (기본적으로 disc, circle 등 가능)
   },
   '& ul': {
     paddingLeft: '20px', // 리스트 항목에 왼쪽 여백을 줍니다.
