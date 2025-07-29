f2e88451f490f905c777bf958f26dbed91b73820
조찬익 <fnjels09@gmail.com>
2025-07-29 14:11:02 +0900

    blog redirect

--- packages/blog/next.config.mjs
+++ packages/blog/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/main',
+        destination: '/pinetree/study',
         permanent: true,
       },
     ];
